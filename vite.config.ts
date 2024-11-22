9eb48cd868ead0ffc3e50e0b3f5fc396befe0ec8
guilherme <guilhermephellippe@outlook.com>
2024-11-22 14:21:28 -0300

    [delete] remove .env

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,37 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true, // Ativa o PWA em modo de desenvolvimento
+      },
+      manifest: {
+        name: 'Finance App',
+        short_name: 'App',
+        description: 'A Progressive Web App built with Vite',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        start_url: '/',
+        display: 'standalone',
+        icons: [
+          {
+            src: 'https://t3.ftcdn.net/jpg/04/93/95/60/360_F_493956028_lkyGavS5R600BEJRlKjDGeECew6nY1OQ.jpg',
+            sizes: '192x192',
+            type: 'image/jpg',
+          },
+          {
+            src: 'https://t3.ftcdn.net/jpg/04/93/95/60/360_F_493956028_lkyGavS5R600BEJRlKjDGeECew6nY1OQ.jpg',
+            sizes: '512x512',
+            type: 'image/jpg',
+          },
+        ],
+      },
+    }),
+  ],
 })
